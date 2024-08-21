d7355ad5c74c0336f2cd1677e203690dbbcf0848
hmxs <hmxsqaq@163.com>
2024-08-21 22:49:23 +0800

    perf: optimize bindableProperty

---
+++ Assets/Scripts/Test/Bindable/BindablePropertyTest.cs
@@ -0,0 +1,27 @@
+﻿using Hmxs.Toolkit.Core.Bindable;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Test.Bindable
+{
+    public class BindablePropertyTest : MonoBehaviour
+    {
+        [SerializeField] private BindableProperty<int> value1;
+        [SerializeField] private BindableProperty<int> value2 = new(10);
+        [SerializeField] private BindableProperty<int> value3 = new(20, v => Debug.Log($"Value3 changed: {v}"));
+
+        private void Start()
+        {
+            value1.onValueChanged.AddListener(v => Debug.Log($"Value1 changed: {v}"));
+            value2.onValueChanged.AddListener(v => Debug.Log($"Value2 changed: {v}"));
+        }
+
+        [Button]
+        private void Test()
+        {
+            value1.Value++;
+            value2.Value++;
+            value3.Value++;
+        }
+    }
+}
